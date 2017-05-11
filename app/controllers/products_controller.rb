f11177b105a5c4e02e01ad792c99bb9b134c68d0
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-05-10 20:15:30 -0400

    added form, make sure it works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,13 @@ def products_method
   render "products_view.html.erb"
 end
 
+def new_product_method
+  render "new_product_view.html.erb"
+end
+
+def create_product_method
+  @new_product = Product.create(name: params[:name_param], price: params[:price_param], image: params[:image_param], description: params[:description_param])
+  render "create_product_view.html.erb"  
+end
+
 end
