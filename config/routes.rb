7880911e7a626eb292e0f5082b22e093427a0561
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-05-10 21:05:44 -0400

    added a create page for the form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products", to: 'products#products_method'
   get "/new_product", to: 'products#new_product_method'
-  get "/product_created", to: 'products#create_product_method'
+  post "/product_created", to: 'products#create_product_method'
 end
